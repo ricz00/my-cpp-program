4d2de0e5d7d5d012ef0c1ddc2fcbdd85e1c36e41
ricz00 <157664577+ricz00@users.noreply.github.com>
2024-02-01 12:44:39 +0800

    Add files via upload

---
+++ program/program21.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks, sum = 0, i = 0;
+    float ave;
+    
+    while(true) {
+    	cout<<"Enter grade: ";
+    	cin>>marks;
+    	
+    	if(marks < 0 || marks > 100)
+    	    break;
+    	    
+    	sum = sum + marks;
+		i++;    
+	}
+    
+    cout<<"Value of couter: "<<i<<"\nTotal sum is "<<sum<<"\n";
+    ave = (float)sum / i;
+    cout<<"Average is "<<ave;
+}
