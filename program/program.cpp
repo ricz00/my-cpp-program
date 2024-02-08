a607590458f023cdc99505f9b06aca85410ff7a9
ricz00 <157664577+ricz00@users.noreply.github.com>
2024-02-08 12:06:16 +0800

    Add files via upload

--- program/program.cpp
+++ program/program.cpp
@@ -1,1 +1,25 @@
+#include<iostream> 
+#include<iomanip>
+#include<cstdlib>
+using namespace std;
 
+int squareByValue(int);
+void squareByReference(int &);
+
+int main(){
+	int x = 2, z = 4;
+   	cout<<"x = "<<x<<"before squareByValue\n";
+	cout<<"Value returned by SquareByValue: "<<squareByValue(x)<<endl;
+	cout<<"x"<<x<<"after SquareByValue\n"<<endl;
+	cout<<"z = "<<z<<"befoere SquareByReference"<<endl;
+	squareByReference(z);
+	cout<<"z = "<<z<<"after SquareByReference"<<endl;
+}
+
+int squareByValue(int num){
+	return num * num;
+}
+
+void squareByReference(int &num){
+    num *= num;	
+}
